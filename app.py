f3b3280003b6a2701bf83ab1d1a38f39d033f078
Lucas <lucasca95@yahoo.com.ar>
2019-10-20 08:59:44 -0300

    .

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask (__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if (request.method=='GET'):
+        #Dar index.html
+        return render_template('index.html')
+    else:
+        f_pantalla=request.form['f_pantalla']
+        #Si venimos del index ir a pantalla 1
+        if(f_pantalla == 'index'):
+            return render_template('pantalla1.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5555)
