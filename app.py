5b5f78e9fa8531f3a21c109a0cded0c13560f7ce
Lucas <lucasca95@yahoo.com.ar>
2019-10-20 08:18:28 -0300

    .

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask (__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    return "Server andando"
-
+    return "Server andando2"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5555)
