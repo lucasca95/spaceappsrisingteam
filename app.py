8e29b573b585a9f54e62fc12d08c5cb9f434d074
Lucas <lucasca95@yahoo.com.ar>
2019-10-20 08:35:16 -0300

    .

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask (__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Server andando2"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5555)
