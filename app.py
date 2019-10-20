aa20bf5b4c412804a9b1b62ace01062f62f85204
Lucas <lucasca95@yahoo.com.ar>
2019-10-20 10:04:11 -0300

    .

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def index():
         #Si venimos del index ir a pantalla 1
         if(f_pantalla == 'index'):
             return render_template('pantalla1.html')
-        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5555)
